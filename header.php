f5702215b1b89e97cbfafcaafda3ef17f4b36719
RihannaCouto <rihannaacouto@gmail.com>
2026-07-29 17:49:23 +0000

    2

--- header.php
+++ header.php
@@ -42,6 +42,6 @@
             <div class="container">
                 <div class="masthead-subheading">Welcome To Our Studio!</div>
                 <div class="masthead-heading text-uppercase">It's Nice To Meet You</div>
-                <a class="btn btn-primary btn-xl text-uppercase" href="formUsuario.php">Cadastre-se</a>
+                <a class="btn btn-primary btn-xl text-uppercase" href="#services">Tell Me More</a>
             </div>
         </header>
